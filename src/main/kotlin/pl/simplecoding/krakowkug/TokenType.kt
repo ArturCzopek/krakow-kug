503d09b4c9ab0260b930940a54499d1e9d75ba6f
ArturC <artur.m.czopek@metrosoft.com>
2018-07-11 19:07:52 +0200

    08. TOKEN_TYPE - enum, companion, when

---
+++ src/main/kotlin/pl/simplecoding/krakowkug/TokenType.kt
@@ -0,0 +1,20 @@
+package pl.simplecoding.krakowkug
+
+
+/**
+ * @author Artur Czopek
+ * @url http://simplecoding.pl/krakow-kug
+ */
+
+enum class TokenType {
+    GREEN,
+    RED;
+
+    companion object {
+        fun fromString(value: String) = when (value) {
+            "green" -> GREEN
+            "red" -> RED
+            else -> throw UnsupportedOperationException()
+        }
+    }
+}
